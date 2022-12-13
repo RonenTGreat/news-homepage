59d9306fbc074f51513a4400a600e24bb362a2ff
ronentgreat <ronenh53@gmail.com>
2022-12-13 20:16:47 +0000

    added hover effect to link

--- src/components/new-item/new-item.component.jsx
+++ src/components/new-item/new-item.component.jsx
@@ -3,7 +3,7 @@ import './new-item.styles.css'
 const NewItem = (props) => {
   return ( 
     <section className="new-item_section">
-      <h3 className="new-item__header">{props.new_item_header}</h3>
+      <a href='headline' className="new-item__header">{props.new_item_header}</a>
       <p className="new-item__paragraph">{props.new_item_paragraph}</p>
     </section>
    );
