a1617fafed38e2f6c97a4e57fa8eadd913df7f38
ronentgreat <ronenh53@gmail.com>
2022-12-13 20:17:22 +0000

    added container

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 
 import Footer from './components/footer/footer.component';
 import Header from './components/header/header.component';
+import Container from './components/container/container.component';
 
 function App() {
   return (
     <div className="App">
     <Header />
+    <Container />
     <Footer />
     </div>
   );
