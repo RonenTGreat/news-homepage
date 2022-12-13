741910f15165bfb427e8e19d37b9b52e61687f67
ronentgreat <ronenh53@gmail.com>
2022-12-13 11:19:11 +0000

    created footer component and added it to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
+import Footer from './assets/components/footer/footer.component';
+
 function App() {
   return (
     <div className="App">
-      Hello World
+    <Footer />
     </div>
   );
 }
---
+++ src/assets/components/footer/footer.component.jsx
@@ -0,0 +1,11 @@
+import "./footer.styles.css"
+
+const Footer = () => {
+  return (
+    <div className="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer" >Frontend Mentor</a>.
+      Coded by <a href="https://twitter.com/Ronen_T_G">Ronen Hammond</a>.
+    </div>
+  );
+}
+export default Footer;
