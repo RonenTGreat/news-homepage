7e4b2650dc87594393762a1b58d30717d47649a9
ronentgreat <ronenh53@gmail.com>
2022-12-14 13:06:46 +0000

    added target blank to my link name

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -3,8 +3,8 @@ import "./footer.styles.css"
 const Footer = () => {
   return (
     <div className="attribution">
-      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer" >Frontend Mentor</a>.
-      Coded by <a href="https://twitter.com/Ronen_T_G">Ronen Hammond</a>.
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" >Frontend Mentor</a>.
+      Coded by <a href="https://twitter.com/Ronen_T_G" target="_blank" rel="noopener noreferrer" >Ronen Hammond</a>.
     </div>
   );
 }
