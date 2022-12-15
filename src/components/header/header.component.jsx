e0aabbce829b05946ee44f8acab9cc98e47f578c
ronentgreat <ronenh53@gmail.com>
2022-12-15 13:21:17 +0000

    added sidebar just to style it properly

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,13 +1,30 @@
 import "./header.styles.css"
 
+import iconMenu from "../../assets/images/icon-menu.svg"
+
+import iconMenuClose from "../../assets/images/icon-menu-close.svg"
+
 import logo from "../../assets/images/logo.svg"
 
+
+
 const Header = () => {
+
+  function openNav() {
+    document.querySelector(".sidenav").style.width = "400px";
+    document.querySelector(".btn_nav").style.display = "none";
+  }
+
+  function closeNav() {
+    document.querySelector(".sidenav").style.width = "0";
+    document.querySelector(".btn_nav").style.display = "block";
+  }
+
   return (
     <header className="header">
       <a href="logo"><img src={logo} className="logo" alt="logo" /></a>
 
-      <div>
+      <nav className="nav_css">
         <ul className="nav_list">
           <li className="nav_item"><a className="nav_link" href="home">Home</a></li>
           <li className="nav_item"><a className="nav_link" href="new">New</a></li>
@@ -15,6 +32,26 @@ const Header = () => {
           <li className="nav_item"><a className="nav_link" href="trending">Trending</a></li>
           <li className="nav_item"><a className="nav_link" href="categories">Categories</a></li>
         </ul>
+      </nav>
+
+
+
+      {/* Hamburger Menu */}
+      <button className="btn_nav" onClick={openNav}>
+        <img src={iconMenu} alt="menu bar" />
+      </button>
+
+      {/* Mobill Menu */}
+
+      <div className="sidenav">
+        <button className="closebtn" onClick={closeNav}>
+          <img src={iconMenuClose} alt="" />
+        </button>
+        <a href="0">Home</a>
+        <a href="0">New</a>
+        <a href="0">Popular</a>
+        <a href="0">Trending</a>
+        <a href="0">Categories</a>
       </div>
     </header>
   );
