cdf14c4b7fa0e5ff546da519270877da50114d33
ronentgreat <ronenh53@gmail.com>
2022-12-19 00:03:17 +0000

    Updated README

--- README.md
+++ README.md
@@ -34,8 +34,8 @@ Users should be able to:
 
 ### Links
 
-- Solution URL: [Add solution URL here](https://your-solution-url.com)
-- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+- Solution URL: [Solution URL here](https://www.frontendmentor.io/solutions/news-homepage-4YNJ7_auVz)
+- Live Site URL: [Live site URL here](https://ronennewshomepage.vercel.app/)
 
 ## My process
 
--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -2,10 +2,10 @@ import "./footer.styles.css"
 
 const Footer = () => {
   return (
-    <div className="attribution">
+    <footer className="attribution">
       Challenge by <a href="https://www.frontendmentor.io?ref=challenge" >Frontend Mentor</a>.
       Coded by <a href="https://twitter.com/Ronen_T_G" target="_blank" rel="noopener noreferrer" >Ronen Hammond</a>.
-    </div>
+    </footer>
   );
 }
 export default Footer;
